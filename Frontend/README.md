9d595e3b03e6bdec0500c816513fc9f668c814ce
Kush Shaileshkumar Sutaria <ks428142@dal.ca>
2023-07-14 19:48:44 -0300

    test

---
+++ Frontend/README.md
@@ -0,0 +1,39 @@
+<!--- The following README.md sample file was adapted from https://gist.github.com/PurpleBooth/109311bb0361f32d87a2#file-readme-template-md by Gabriella Mosquera for academic use ---> 
+
+
+# Sneakers E-commerce
+
+* *Date Created*: 22 MAY 2023
+* *Last Modification Date*: 20 JUNE 2023
+* *Hosted URL*: [https://remarkable-sprite-930713.netlify.app/](https://remarkable-sprite-930713.netlify.app/)
+* *Git URL*: https://git.cs.dal.ca/dlad/csci-5709-grp-14
+
+## Authors
+
+* [Deep Pravinbhai Lad](dp658583@dal.ca) - *(Developer)*
+* [Kush Sutaria](ks428142@dal.ca) - *(Developer)*
+* [Dhruv Rajesh Kothari](dh801135@dal.ca) - *(Developer)*
+* [Rohin Ashwinkumar Sharma](rh851598@dal.ca) - *(Developer)*
+* [Jaimin Kanubhai Desai](jm690660@dal.ca) - *(Developer)*
+
+
+## Deployment
+
+Deployment platform name: [Netlify](https://app.netlify.com)
+
+Link to hosted project : [https://remarkable-sprite-930713.netlify.app/](https://remarkable-sprite-930713.netlify.app/)
+
+## Built With
+
+* [npm](https://www.npmjs.com/) - Package manager for node.js
+* [create-react-app](https://create-react-app.dev/) - Tool for creating React applications
+* [nodejs](https://nodejs.org/en) - Runtime used
+* [React.js](https://react.dev/) - The web framework used
+* [react-router-dom](https://reactrouter.com/) - Routing library for React
+* [ant-design](https://ant.design/) - UI library for React applications
+
+### References
+
+[1] “Get the current route using react router,” Datainfinities, [Online]. Available: https://www.datainfinities.com/41/get-the-current-route-using-react-router [Accessed: June 19, 2023]. 
+
+[2] “How to - position text over an image,” Tutorials, [Online]. Available: https://www.w3schools.com/howto/howto_css_image_text.asp [Accessed: June 19, 2023]. 
---
+++ Frontend/src/Pages/UserManagement/Login.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import './Login.css';
+import Header from '../../Components/Header';
+import Footer from '../../Components/Footer';
+
+
+const LoginPage = () => {
+const navigate = useNavigate();
+
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').matches(/^[a-zA-Z0-9._-]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/, 'Invalid email address'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const handleSubmit = () => {
+    console.log('Login button clicked!');
+    alert('Login Successful!');
+  };
+
+  const registerButton = () => {
+    console.log('Register button clicked');
+    navigate('/signup');
+  };
+
+  const forgotPassword = () => {
+    console.log('Forgot button clicked');
+  };
+  return (
+    <div>        
+        <Header/>
+    <div className="login-page">
+      <div className="login-container">
+        <h1>Login</h1>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+          <Form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field type="email" id="email" name="email" />
+              <ErrorMessage name="email" component="div" className="error-message" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field type="password" id="password" name="password" />
+              <ErrorMessage name="password" component="div" className="error-message" />
+            </div>
+            <button type="submit" className="signup-button">
+            Login
+          </button>
+          <div className='signup-form-row'>
+          <button type="button" className="login-button" onClick={registerButton}>
+            Sign Up
+          </button>
+          <button type="button" className="login-button" onClick={forgotPassword}>
+            Forgot Password?
+          </button>
+          </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+    <Footer/>
+    </div>
+  );
+};
+
+export default LoginPage;
