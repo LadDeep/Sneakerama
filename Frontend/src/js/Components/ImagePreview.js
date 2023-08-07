6ea4bb334402f4ab583db2ea0b4e7f7261aa09ea
Deep Pravinbhai Lad <dp658583@dal.ca>
2023-08-06 22:19:24 -0300

    fix: identation and remove comments

--- Frontend/src/js/Components/ImagePreview.js
+++ Frontend/src/js/Components/ImagePreview.js
@@ -1,55 +1,56 @@
-import { DeleteOutlined, LeftSquareFilled, RightSquareFilled } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  LeftSquareFilled,
+  RightSquareFilled,
+} from "@ant-design/icons";
 import React, { useState } from "react";
 import "../../css/ImagePreview.css";
 
 function ImagePreview({ imageFiles, onChange }) {
   const [currentStep, setCurrentStep] = useState(0);
   const handleCarousal = (step) => {
-    if(step>=0 && step<imageFiles.length){
-      console.log("step",step)
-
-      setCurrentStep(step)
-    }
+    if (step >= 0 && step < imageFiles.length) 
+      setCurrentStep(step);
   };
-  const handleRemoveImage =()=>{
-    setCurrentStep(currentStep>0?currentStep-1:0)
-    onChange(currentStep)
-  }
+  const handleRemoveImage = () => {
+    setCurrentStep(currentStep > 0 ? currentStep - 1 : 0);
+    onChange(currentStep);
+  };
   return (
     <>
-      {imageFiles.length===0 ?
-    <img
-    src={"../../images/add-photos.jpg"}
-    alt="Selected"
-    style={{
-      objectFit: "cover",
-      width: "300px",
-      height: "300px",
-      marginTop: "10px",
-    }}
-  />:(
-<div className="carousal-container">
-      <LeftSquareFilled className="carousal-icon" disabled={currentStep <= 0} onClick={()=>handleCarousal(currentStep-1)}/>
-      {imageFiles &&
-          (<div className="carousal-image-container">
-          <img
-            src={imageFiles[currentStep]}
-            alt="Selected"
-            className="carousal-image"
-            // style={{
-            //   objectFit: "cover",
-            //   width: "300px",
-            //   height: "300px",
-            //   marginTop: "10px",
-            // }}
-            />
-            <DeleteOutlined onClick={handleRemoveImage} className="delete-icon"/>
+      {imageFiles.length === 0 ? (
+        <img
+          src={"../../images/add-photos.jpg"}
+          alt="Selected"
+          className="carousal-image"
+        />
+      ) : (
+        <div className="carousal-container">
+          <LeftSquareFilled
+            className="carousal-icon"
+            disabled={currentStep <= 0}
+            onClick={() => handleCarousal(currentStep - 1)}
+          />
+          {imageFiles && (
+            <div className="carousal-image-container">
+              <img
+                src={imageFiles[currentStep]}
+                alt="Selected"
+                className="carousal-image"
+              />
+              <DeleteOutlined
+                onClick={handleRemoveImage}
+                className="delete-icon"
+              />
             </div>
-            )}
-      <RightSquareFilled className="carousal-icon" disabled={currentStep > imageFiles.length - 1} onClick={()=>handleCarousal(currentStep+1)}/>
-            </div>
-  )  
-}
+          )}
+          <RightSquareFilled
+            className="carousal-icon"
+            disabled={currentStep > imageFiles.length - 1}
+            onClick={() => handleCarousal(currentStep + 1)}
+          />
+        </div>
+      )}
     </>
   );
 }
